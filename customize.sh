0cc95e4d4ea96f167524089bab3969d5726e84ea
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2022-01-23 17:18:11 +0900

    Add files via upload
    Resampling quality changes from attenuation 140dB & length 320 to 160dB & 480.

--- customize.sh
+++ customize.sh
@@ -3,5 +3,8 @@
 if "$IS64BIT"; then
  :
 else
-  sed -i 's/ro\.audio\.usb\.period_us=.*$/ro\.audio\.usb\.period_us=5600/' "$MODPATH/system.prop"
+  sed -i \
+    -e 's/ro\.audio\.usb\.period_us=.*$/ro\.audio\.usb\.period_us=5600/' \
+    -e 's/ro\.audio\.resampler\.psd\.halflength=.*$/ro\.audio\.resampler\.psd\.halflength=320/' \
+        "$MODPATH/system.prop"
 fi
