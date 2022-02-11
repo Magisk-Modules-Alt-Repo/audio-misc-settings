c7582ad9f624a16b2efcac23e860977eac1ed58a
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2022-02-11 19:01:49 +0900

    Add files via upload
    Shortened the half filter length of resampling for MTK old SoC's not to  stutter.

--- customize.sh
+++ customize.sh
@@ -1,10 +1,19 @@
-# Replace the value of "ro.audio.usb.period_us"
+# Replace system property values for some low performance SoC's
+
+function replaceSystemProps()
+{
+    sed -i \
+        -e 's/ro\.audio\.usb\.period_us=.*$/ro\.audio\.usb\.period_us=5600/' \
+        -e 's/ro\.audio\.resampler\.psd\.halflength=.*$/ro\.audio\.resampler\.psd\.halflength=320/' \
+            "$MODPATH/system.prop"
+}
 
 if "$IS64BIT"; then
- :
+    case "`getprop ro.board.platform`" in
+        mt67[56]? )
+            replaceSystemProps
+            ;;
+    esac
 else
-  sed -i \
-    -e 's/ro\.audio\.usb\.period_us=.*$/ro\.audio\.usb\.period_us=5600/' \
-    -e 's/ro\.audio\.resampler\.psd\.halflength=.*$/ro\.audio\.resampler\.psd\.halflength=320/' \
-        "$MODPATH/system.prop"
+    replaceSystemProps
 fi
