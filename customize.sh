7c771a8659d8ef8ba582b55eca1f9726f9efe381
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2022-03-16 20:51:06 +0900

    Add files via upload
    Changed resampling parameters for A12 and later (not having a certain resampling bug) to reduce the computational cost of resampling and raise its quality a little.

--- customize.sh
+++ customize.sh
@@ -1,11 +1,14 @@
-# Replace system property values for some low performance SoC's
+# Replace system property values for old Androids and some low performance SoC's
 
 function replaceSystemProps()
 {
     sed -i \
         -e 's/ro\.audio\.usb\.period_us=.*$/ro\.audio\.usb\.period_us=5600/' \
-        -e 's/ro\.audio\.resampler\.psd\.halflength=.*$/ro\.audio\.resampler\.psd\.halflength=320/' \
             "$MODPATH/system.prop"
+    sed -i \
+        -e 's/ro\.audio\.usb\.period_us=.*$/ro\.audio\.usb\.period_us=5600/' \
+        -e 's/ro\.audio\.resampler\.psd\.halflength=.*$/ro\.audio\.resampler\.psd\.halflength=320/' \
+            "$MODPATH/system.prop-workaround"
 }
 
 if "$IS64BIT"; then
@@ -16,4 +19,14 @@ if "$IS64BIT"; then
     esac
 else
     replaceSystemProps
+fi
+
+# AudioFlinger's resampler has a bug on an Android OS of which version is less than 12.
+# This bug makes the resampler to distort audible audio output by wrong aliasing processing
+#   when specifying a transition band around or higher than the Nyquist frequency
+
+if [ "`getprop ro.system.build.version.release`" -lt "12" ]; then
+    mv -f "$MODPATH/system.prop-workaround" "$MODPATH/system.prop"
+else
+    rm -f "$MODPATH/system.prop-workaround"
 fi
--- service.sh
+++ service.sh
@@ -3,20 +3,30 @@
 # sleep 31 secs needed for "settings" commands to become effective
 # and make volume medial steps to be 100 if a volume steps facility is used
 
+function which_resetprop_command()
+{
+    type resetprop 1>"/dev/null" 2>&1
+    if [ $? -eq 0 ]; then
+        echo "resetprop"
+    else
+        type resetprop_phh 1>"/dev/null" 2>&1
+        if [ $? -eq 0 ]; then
+            echo "resetprop_phh"
+        else
+            return 1
+        fi
+    fi
+    return 0
+}
+
 function additionalSettings()
 {
     if [ "`getprop persist.sys.phh.disable_audio_effects`" = "0" ]; then
-        
-        type resetprop 1>/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            resetprop ro.audio.ignore_effects true
+        resetprop_command="`which_resetprop_command`"
+        if [ -n "$resetprop_command" ]; then
+            "$resetprop_command" ro.audio.ignore_effects true
         else
-            type resetprop_phh 1>/dev/null 2>&1
-            if [ $? -eq 0 ]; then
-                resetprop_phh ro.audio.ignore_effects true
-            else
-                return 1
-            fi
+            return 1
         fi
         
         if [ "`getprop init.svc.audioserver`" = "running" ]; then
@@ -24,13 +34,9 @@ function additionalSettings()
         fi
         
     elif [ "`getprop ro.system.build.version.release`" -ge "12" ]; then
-        
-        local audioHal
-        setprop ctl.restart audioserver
-        audioHal="$(getprop |sed -nE 's/.*init\.svc\.(.*audio-hal[^]]*).*/\1/p')"
-        setprop ctl.restart "$audioHal" 1>"/dev/null" 2>&1
-        setprop ctl.restart vendor.audio-hal-2-0 1>"/dev/null" 2>&1
-        setprop ctl.restart audio-hal-2-0 1>"/dev/null" 2>&1
+        if [ "`getprop init.svc.audioserver`" = "running" ]; then
+            setprop ctl.restart audioserver
+        fi
         
     fi
     settings put system volume_steps_music 100
